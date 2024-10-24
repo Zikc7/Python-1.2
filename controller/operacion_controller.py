03e9d55bcbd66108ff3c440b556c3b70d870c59d
Santiago <santimir12@gmail.com>
2024-10-24 01:31:25 -0300

    Se mejora el tratamiento de errores y tratamiento de errores menores

--- controller/operacion_controller.py
+++ controller/operacion_controller.py
@@ -1,5 +1,6 @@
 from models.operacion_model import Operacion
 from helpers.utils import limpiar_pantalla
+from helpers.error_treatment import tratamiento_opc_cambiar_op
 from core.historial_operaciones import guardar_historial, mostrar_historial, eliminar_historial,modificar_op
 
 class OperacionModel:
@@ -35,23 +36,26 @@ class OperacionModel:
                 case 7:
                     cls.op_modif()
                 case 0:
+                    limpiar_pantalla()
                     print("Gracias por usar nuestra aplicacion!!")
                     break
                 case _:
+                    limpiar_pantalla()
                     print("Selecciones una opcion habilitada: ")
+
 
     @classmethod
     def seleccion_operacion(cls) -> float:
         """Funcion donde se solicita el numero que nos dira que operacion usar"""
-        try:
-            opcion: float = float(
-                input("Seleccione la operacion que desea ejecutar: "))
-            return opcion
-        except ValueError as ex:
-            limpiar_pantalla()
-            print(f"{ex} Se esperaba un número")
-            return cls.seleccion_operacion()
-
+        while True:
+            try:
+                opcion: float = float(
+                    input("Seleccione la operacion que desea ejecutar: "))
+                return opcion
+                break
+            except ValueError as ex:
+                limpiar_pantalla()
+                print(f"{ex} Se esperaba un número")
     @classmethod
     def seleccion_numeros(cls) -> "Operacion":
         """Segun la operacion les solicita los operandos al usuario,
@@ -67,10 +71,24 @@ class OperacionModel:
             except ValueError as ex:
                 limpiar_pantalla()
                 print(f"{ex} Se esperaba un número")
-                
+    @classmethod
+    def seleccion_numeros_modificar(cls) -> "Operacion":
+        """Segun la operacion les solicita los operandos al usuario,
+        si esta es una division verifica que no sean 0"""
+        while(True):
+            try:
+                num1: float = float(input("Ingrese el primer operando: "))
+                num2: float = float(input("Ingrese el segundo operando: "))
+                nueva_op = Operacion(operando_1=num1, operando_2=num2)
+                return nueva_op
+                break
+            except ValueError as ex:
+                limpiar_pantalla()
+                print(f"{ex} Se esperaba un número")
+                    
 
     @classmethod
-    def suma(cls, nueva_op: "Operacion") -> str:
+    def suma(cls, nueva_op: "Operacion") -> "Operacion":
         """Funcion donde se realiza y devuelva el resusltado de la suma"""
         limpiar_pantalla()
         resultado: str = f"{nueva_op.operando_1} + {nueva_op.operando_2} = {
@@ -80,7 +98,7 @@ class OperacionModel:
         return nueva_op
 
     @classmethod
-    def resta(cls, nueva_op: "Operacion") -> str:
+    def resta(cls, nueva_op: "Operacion") -> "Operacion":
         """Operacion donde se realiza y devuelve el resultado de la resta"""
         limpiar_pantalla()
         resultado: str = f"{nueva_op.operando_1} - {nueva_op.operando_2} = {
@@ -89,7 +107,7 @@ class OperacionModel:
         print(nueva_op.resultado_op)
         return nueva_op
     @classmethod
-    def division(cls, nueva_op: "Operacion") -> str:
+    def division(cls, nueva_op: "Operacion") -> "Operacion":
         """Operacion donde se realiza y devuelve el resultado de la división"""
         limpiar_pantalla()
         while True:
@@ -105,7 +123,7 @@ class OperacionModel:
                 return cls.division(cls.seleccion_numeros())
 
     @classmethod
-    def multiplicacion(cls, nueva_op: "Operacion") -> Operacion:
+    def multiplicacion(cls, nueva_op: "Operacion") -> "Operacion":
         """Operacion donde se realiza y devuelve el resultado de la multiplicación"""
         limpiar_pantalla()
         resultado: str = f"{nueva_op.operando_1} * {nueva_op.operando_2} = {
@@ -123,38 +141,19 @@ class OperacionModel:
         print("3: División")
         print("4: Multiplicación")
         opcion: float = cls.seleccion_operacion()
-        while True:
-            match opcion:
-                case 1:
-                        try:
-                            modificar_op(cls.suma(cls.seleccion_numeros()))
-                            break
-                        except ValueError as ex:
-                            print(f"{ex}: Error al modificar la operación. Ingrese un número válido.")
-                        except IndexError as ex:
-                            print(f"{ex}: Error al modificar la operación. Ingrese un número de operacion valido válido.")
-                case 2:
-                        try:
-                            modificar_op(cls.resta(cls.seleccion_numeros()))
-                            break
-                        except ValueError as ex:
-                            print(f"{ex}: Error al modificar la operación. Ingrese un número válido.")
-                            modificar_op(cls.resta(cls.seleccion_numeros()))
-
-                case 3:
-                        try:
-                            modificar_op(cls.division(cls.seleccion_numeros()))
-                            break
-                        except ValueError as ex:
-                            print(f"{ex}: Error al modificar la operación. Ingrese un número válido.")
-                            modificar_op(cls.division(cls.seleccion_numeros()))
-                case 4:
-                        try:
-                            modificar_op(cls.multiplicacion(cls.seleccion_numeros()))
-                            break
-                        except ValueError as ex:
-                            print(f"{ex}: Error al modificar la operación. Ingrese un número válido.")
-                            modificar_op(cls.multiplicacion(cls.seleccion_numeros()))
-                case _:
-                    print("Ingrese un numero de operacion valido")
-                    opcion: float = cls.seleccion_operacion()
+        match opcion:
+            case 1:
+                    op_nums :"Operacion" = cls.seleccion_numeros()
+                    tratamiento_opc_cambiar_op(cls.suma,op_nums)
+            case 2:
+                    op_nums :"Operacion" = cls.seleccion_numeros()
+                    tratamiento_opc_cambiar_op(cls.resta,op_nums)
+            case 3:
+                    op_nums :"Operacion" = cls.seleccion_numeros()
+                    tratamiento_opc_cambiar_op(cls.division,op_nums)
+            case 4:
+                    op_nums :"Operacion" = cls.seleccion_numeros()
+                    tratamiento_opc_cambiar_op(cls.multiplicacion,op_nums)
+            case _:
+                print("Ingrese un numero de operacion valido")
+                cls.op_modif()
---
+++ helpers/error_treatment.py
@@ -0,0 +1,15 @@
+ 
+from core.historial_operaciones import modificar_op
+#from controller.operacion_controller import OperacionModel
+
+def tratamiento_opc_cambiar_op(operaciones_varias,seleccion_num_op) -> None :
+    """Funcion encargada de recibir la modificacion de una operacion y tratar sus posibles errores"""
+    try:
+
+        modificar_op(operaciones_varias(seleccion_num_op))
+    except ValueError as ex:
+        print(f"{ex}: Error al modificar la operación. Ingrese un número válido.")
+        tratamiento_opc_cambiar_op(operaciones_varias, seleccion_num_op)
+    except IndexError as ex:
+        print(f"{ex}: Error al modificar la operación. Ingrese un número de operacion valido válido.")
+        tratamiento_opc_cambiar_op(operaciones_varias, seleccion_num_op)
